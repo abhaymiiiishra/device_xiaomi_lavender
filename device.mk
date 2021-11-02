153d275d2a3aba0b8dec1d07a425bfb5df181f8b
Albert I <kras@raphielgang.org>
2021-11-02 21:47:53 +0530

    lavender: Makefile: Inherit several Android Go configurations
    Beneficial for everyone, even on non-Go devices.
    
    Change-Id: I088fd00500ce10df5f413cb762aaed2ec00f5dfc
    Signed-off-by: wHo-EM-i <ehteshammalik18998@gmail.com>
    Signed-off-by: Prashant <prashant33968@gmail.com>
    Signed-off-by: OmarAlCoptan <omarcoptan9@gmail.com>

--- device.mk
+++ device.mk
@@ -329,6 +329,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     NotchBarKiller
 
+# Inherit several Android Go Configurations(Beneficial for everyone, even on non-Go devices)
+PRODUCT_USE_PROFILE_FOR_BOOT_IMAGE := true
+PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := frameworks/base/config/boot-image-profile.txt
+
 # Power
 PRODUCT_PACKAGES += \
      android.hardware.power-service.lavender-libperfmgr
