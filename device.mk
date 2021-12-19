dfd05562024b8b2a009dca3fa0c1be774efc4716
jhenrique09 <jhsv09@gmail.com>
2021-12-19 01:12:21 -0300

    lavender: Remove references for antradio_library
    Signed-off-by: OmarAlCoptan <omarcoptan9@gmail.com>

--- device.mk
+++ device.mk
@@ -127,8 +127,7 @@ PRODUCT_COPY_FILES += \
 
 # ANT+
 PRODUCT_PACKAGES += \
-    AntHalService-Soong \
-    com.dsi.ant@1.0.vendor
+    AntHalService
 
 # Component overrides
 PRODUCT_COPY_FILES += \
