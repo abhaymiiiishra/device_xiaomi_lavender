84c9d7c0d753b45f5cdd1907b9ea3954c716ca8a
KenHV <yo@kenharris.xyz>
2022-05-21 14:03:59 +0000

    lavender: fix screen recorder lag
    Signed-off-by: ImPrashantt <prashant33968@gmail.com>
    Signed-off-by: OmarAlCoptan <omarcoptan9@gmail.com>

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -137,6 +137,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     vendor.gralloc.enable_fb_ubwc=1 \
     vendor.display.enable_default_color_mode=0 \
     ro.vendor.display.cabl=0 \
+    debug.sdm.support_writeback=0
 
 # DRM
 PRODUCT_PROPERTY_OVERRIDES += \
