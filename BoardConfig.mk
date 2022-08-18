ebc0846def3e6b621b55fb38a726c88f0afe5999
Sal Savage <salsavage@google.com>
2022-08-18 12:44:49 +0530

    lavender: prop: Set default the Bluetooth class of device
    The class of device is a string with a list of uint8t values:
    90,2,12
    
    The meaning is as follows:
    {Service Field, Major class, Minor class}
    
    Service Field: 0x5A -> 90
        Bit 17: Networking
        Bit 19: Capturing
        Bit 20: Object Transfer
        Bit 22: Telephony
    MAJOR_CLASS: 0x02 -> 2 (Phone)
    MINOR_CLASS: 0x0C -> 12 (Smart Phone)
    
    Bug: 217452259
    Test: make -j; -- check that bluetooth.device.class_of_device is
    correct
    
    Change-Id: I07e83ca352ff969be0e53a0f63b6c940b6c34dfb
    Signed-off-by: karthik1896 <karthik1896@outlook.com>
    Signed-off-by: Vitorgl2003 <vitorgl.2003@gmail.com>
    Signed-off-by: ImPrashantt <prashant33968@gmail.com>
    Signed-off-by: OmarAlCoptan <omarcoptan9@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -142,6 +142,7 @@ TARGET_COPY_OUT_VENDOR := vendor
 # Properties
 TARGET_ODM_PROP += $(DEVICE_PATH)/odm.prop
 TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
+TARGET_SYSTEM_EXT_PROP += $(DEVICE_PATH)/system_ext.prop
 
 # QCOM
 BOARD_USES_QCOM_HARDWARE := true
