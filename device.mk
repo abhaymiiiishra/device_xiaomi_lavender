477a9e5c2d853fe8e792ffa0fadb7d53bcea860c
Lunarixus <Nathanb644@hotmail.co.uk>
2022-09-29 08:02:04 +0000

    lavender: Remove unwanted GApps Packages
    Signed-off-by: OmarAlCoptan <omarcoptan9@gmail.com>

--- device.mk
+++ device.mk
@@ -379,6 +379,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     librecovery_updater_lavender
 
+# Remove unwanted packages
+PRODUCT_PACKAGES += \
+    RemovePackages
+
 # RIL
 PRODUCT_PACKAGES += \
     android.hardware.secure_element@1.0 \
