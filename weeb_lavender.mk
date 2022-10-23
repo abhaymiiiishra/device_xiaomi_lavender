d3cf831d3a62e0af99cf5fea5abe344bca801a10
OmarAlCoptan <omarcoptan9@gmail.com>
2022-10-23 14:13:42 +0200

    lavender: update FP from `raven` may
    Signed-off-by: OmarAlCoptan <omarcoptan9@gmail.com>

--- weeb_lavender.mk
+++ weeb_lavender.mk
@@ -30,4 +30,4 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME="lavender" \
     PRIVATE_BUILD_DESC="lavender-user 9 PKQ1.180904.001 V11.0.5.0.PFGMIXM release-keys"
 
-BUILD_FINGERPRINT :="google/walleye/walleye:8.1.0/OPM1.171019.011/4448085:user/release-keys"
+BUILD_FINGERPRINT :="google/raven/raven:12/SP2A.220505.002/8353555:user/release-keys"
